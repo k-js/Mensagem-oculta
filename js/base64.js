7ed87a7d25fa4ba3088c1cb62e9faebd7c925491
Keu Santos <modulodigital69@gmai.com>
2022-04-07 21:51:19 -0300

    javaScript base64

---
+++ js/base64.js
@@ -0,0 +1,37 @@
+function ativaCodifica() {
+    document.getElementById("enviar").value = "Codificar Mensagem!"  
+}
+function ativaDecodifica() {
+    document.getElementById("enviar").value = "Decodificar Mensagem!"    
+}
+
+document.getElementById("enviar").addEventListener("click", codificar)
+document.getElementById("enviar").addEventListener("click", decodificar)
+
+function codificar() {
+
+    const textoDigitado = document.getElementById("message-box").value;
+    const encodedStr = btoa(textoDigitado);
+    const resultado = encodedStr;
+  
+    document.getElementById("resultado").innerHTML =
+    ("<h3 id='titulo2'>Mensagem cifrada:</h3>" +
+      "<textarea rows=5 cols=70>" + resultado + "</textarea><br>");
+}
+
+function decodificar() {
+
+    const textoDigitado = document.getElementById("message-box").value;
+    const encodedStr = atob(textoDigitado);
+    const resultado = encodedStr;
+  
+    document.getElementById("resultado").innerHTML =
+    ("<h3 id='titulo2'>Mensagem cifrada:</h3>" +
+      "<textarea rows=5 cols=70>" + resultado + "</textarea><br>");
+}
+
+document.getElementById("limpar").addEventListener("click", limpar);
+
+  function limpar() {
+    location.reload();
+  }
